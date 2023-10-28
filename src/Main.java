638f45fcceeffe428e9ab38cbab19c5a62a3fa0e
Vyacheslav <vip041961@yandex.ru>
2023-10-28 20:33:43 +0500

    https://github.com/Vyacheslav-2/dz4.2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,53 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+/*Класс Phone.
+Создайте класс Phone, который содержит переменные number, model и weight.
+        Создайте три экземпляра этого класса.
+        Выведите на консоль значения их переменных.
+        Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего.
+        Выводит на консоль сообщение “Звонит {name}”. Метод getNumber – возвращает номер телефона.
+        Вызвать эти методы для каждого из объектов.
+        Добавить конструктор в класс Phone, который принимает на вход три параметра для инициализации переменных класса - number, model и weight.
+        Добавить конструктор, который принимает на вход два параметра для инициализации переменных класса - number, model.
+        Добавить конструктор без параметров.*/
+
+public class Phone {
+    private String number;
+    private String model;
+    private double weight;
+
+    public Phone(String number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public Phone(String number, String model) {
+        this(number, model, 0.0);
+    }
+
+
+    public Phone() {
+        this("", "", 0.0);
+    }
+
+    public void receiveCall(String name) {
+        System.out.println("Звонит " + name);
+    }
+
+    public String getNumber() {
+        return number;
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Phone phone1 = new Phone("123-456-7890", "iPhone X", 0.4);
+        Phone phone2 = new Phone("987-654-3210", "Samsung Galaxy");
+        Phone phone3 = new Phone();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Phone 1: Number - " + phone1.getNumber() + ", Model - " + phone1.model + ", Weight - " + phone1.weight);
+        System.out.println("Phone 2: Number - " + phone2.getNumber() + ", Model - " + phone2.model + ", Weight - " + phone2.weight);
+        System.out.println("Phone 3: Number - " + phone3.getNumber() + ", Model - " + phone3.model + ", Weight - " + phone3.weight);
+
+        phone1.receiveCall("Alice");
+        phone2.receiveCall("Bob");
+        phone3.receiveCall("Charlie");
     }
 }
